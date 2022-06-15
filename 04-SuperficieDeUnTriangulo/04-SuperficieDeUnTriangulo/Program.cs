16ca94b6dd824b5dab0ec9de28a021e155a6b681
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:21:09 -0300

    Add files via upload

---
+++ 04-SuperficieDeUnTriangulo/04-SuperficieDeUnTriangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_SuperficieDeUnTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int c = 0;
+
+            Console.WriteLine("Ingresar base y altura del triangulo: ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = (a * b) / 2;
+
+            Console.WriteLine("La superficie es de: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
